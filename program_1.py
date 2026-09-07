45b021915bb6561542f3166f971b561f3ec045fb
8529saini-vikash <8529saini@gmail.com>
2026-09-07 15:18:37 +0530

    Add files via upload

---
+++ program_1.py
@@ -0,0 +1,19 @@
+def caesar_cipher(text, shift):
+    result = ""
+
+    for ch in text:
+        if ch.isalpha():
+            start = ord('A') if ch.isupper() else ord('a')
+            result += chr((ord(ch) - start + shift) % 26 + start)
+        else:
+            result += ch
+
+    return result
+
+
+text = input("Enter text: ")
+shift = int(input("Enter shift value: "))
+
+encrypted = caesar_cipher(text, shift)
+
+print("Encrypted text:", encrypted)
